f9a82f46e9d63e4e603f79fbebc88ed75a787257
Nicklas Breum <nicklasbreum@gmail.com>
2023-07-26 21:48:21 +0200

    [ideaplugin] Add inclusion listener using ChangesViewWorkflowListener.TOPIC

--- ideaplugin/src/main/java/com/github/nbreum15/monorepotools/extensions/AutomaticCommitMessageProvider.java
+++ ideaplugin/src/main/java/com/github/nbreum15/monorepotools/extensions/AutomaticCommitMessageProvider.java
@@ -3,24 +3,42 @@ package com.github.nbreum15.monorepotools.extensions;
 import com.github.nbreum15.monorepotools.actions.CommitMessageAction;
 import com.github.nbreum15.monorepotools.settings.autocommitmessage.AutoCommitMessageSettingsState;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.ProjectActivity;
+import com.intellij.openapi.vcs.changes.ChangesViewWorkflowManager;
 import com.intellij.vcs.commit.CommitWorkflowUi;
-import com.intellij.vcs.commit.DelayedCommitMessageProvider;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * {@link com.intellij.openapi.vcs.changes.ui.CommitMessageProvider} may be a suitable alternative if this interface is removed.
- */
-public class AutomaticCommitMessageProvider implements DelayedCommitMessageProvider {
+import java.util.concurrent.atomic.AtomicBoolean;
 
+public class AutomaticCommitMessageProvider implements ProjectActivity {
+
+    private final static AtomicBoolean inclusionListenerAdded = new AtomicBoolean(false);
+
+    @Nullable
     @Override
-    public void init(@NotNull Project project, @NotNull CommitWorkflowUi commitWorkflowUi, @Nullable String s) {
-        commitWorkflowUi.addInclusionListener(
-                () -> {
-                    if (AutoCommitMessageSettingsState.getInstance(project).toConfig().automaticCommitMessage()) {
-                        CommitMessageAction.setNewCommitMessage(project);
-                    }
-                },
-                () -> { /* do nothing */ });
+    public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
+        project.getMessageBus().connect().subscribe(ChangesViewWorkflowManager.TOPIC, (ChangesViewWorkflowManager.ChangesViewWorkflowListener) () -> {
+            if (inclusionListenerAdded.compareAndSet(false, true)) {
+                ChangesViewWorkflowManager changesViewWorkflowManager = ChangesViewWorkflowManager.getInstance(project);
+                var commitWorkFlowHandler = changesViewWorkflowManager.getCommitWorkflowHandler();
+
+                if (commitWorkFlowHandler == null) {
+                    return;
+                }
+
+                CommitWorkflowUi ui = commitWorkFlowHandler.getUi();
+                ui.addInclusionListener(
+                        () -> {
+                            if (AutoCommitMessageSettingsState.getInstance(project).toConfig().automaticCommitMessage()) {
+                                CommitMessageAction.setNewCommitMessage(project);
+                            }
+                        },
+                        () -> { /* do nothing */ });
+            }
+        });
+        return null;
     }
 }
